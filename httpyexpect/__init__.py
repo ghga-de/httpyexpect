9e7ba5db1983eb6634386b58191e1bd360257cb5
KerstenBreuer <kersten-breuer@outlook.com>
2022-06-09 08:48:56 +0000

    improve description

--- httpyexpect/__init__.py
+++ httpyexpect/__init__.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""An opinon on how to translate HTTP errors from server to the client side
-and the tools for the implement of that opinion."""
+"""An opinionated way to translate server side HTTP errors to the client side."""
 
 __version__ = "0.1.0"
