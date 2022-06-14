4050b11ae89a4f4ec810d0d96d51e5f0cf01e958
KerstenBreuer <kersten-breuer@outlook.com>
2022-06-14 11:51:58 +0000

    more precise description

--- httpyexpect/models/http_exception.py
+++ httpyexpect/models/http_exception.py
@@ -34,19 +34,19 @@ from pydantic import BaseModel, Extra, Field, constr
 
 class HttpException(BaseModel):
     """
-    An opinionated schema for the response shipped with HTTP exception (non 2xx status codes).
+    An opinionated schema for the response body shipped with HTTP exception (non 2xx status codes).
     """
 
     class Config:
         extra = Extra.forbid
 
-    description: str = Field(
-        ...,
-        description="A human readable message to the client explaining the cause of the exception.",
-    )
     data: Dict[str, Any] = Field(
         ...,
         description="An object containing further details on the exception cause in a machine readable way. Even though this general exception schema does not specify the internal shape of the details object, all exceptions with the same exceptionId should use the same set of properties. This object may be empty (in case no details are required)",
+    )
+    description: str = Field(
+        ...,
+        description="A human readable message to the client explaining the cause of the exception.",
     )
     exceptionId: constr(regex=r"^[a-z][a-zA-Z0-9]{2,39}$") = Field(
         ...,
